598695846c1d6766f96d88a12b0dcac966e252fc
okasha sameer <57588363+Okasha00@users.noreply.github.com>
2024-11-20 16:55:19 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TagHelperDemo.Models;
+
+namespace TagHelperDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Index(Employee e)
+        {
+            return "Name:" + e.Name + "Gender:" + e.Gender + "Age:" + e.Age + "Designation:" + e.Designation + "Salary:" + e.Salary + "Married:"+ e.Married + "Description" + e.Description ;
+
+        }
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
